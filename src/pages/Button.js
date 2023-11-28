7e4b070e0537c8bc0a7114c132f4ca69853ed02f
SeJin317 <121839794+SeJin317@users.noreply.github.com>
2023-11-28 15:24:24 +0900

    Button.js

---
+++ src/pages/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <button className="big-button">잃어버렸어요</button>
+        <button className="big-button">주웠어요</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
