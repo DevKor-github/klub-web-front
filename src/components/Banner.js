d02c9ed20d18062c47d01d22d4b104420fb4dc72
SeJin317 <121839794+SeJin317@users.noreply.github.com>
2023-11-30 17:20:09 +0900

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,45 @@
 // Banner.js
-
 import React from 'react';
-import './Banner.css'; // Import the CSS file for styling
+import styled from 'styled-components';
 
-const Banner = () => {
+const StyledBanner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.2rem;
+  width: 300px; // Adjust the width as needed
+  margin: 0 10px; // Add margin between banners
+  justify-content: space-between;
+
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 8px; // Optional: Add rounded corners to images
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-family: 'Pretendard-Bold', sans-serif;
+    cursor: pointer;
+    white-space: pure;
+    text-decoration: none;
+    color: inherit;
+    margin-top: 10px;
+
+    &:hover {
+      color: rgb(134, 38, 51);
+    }
+  }
+`;
+
+const Banner = ({ image, title }) => {
   return (
-    <div className="banner">
-      <h2>Banner Content</h2>
-      {/* Additional content for the banner */}
-    </div>
+    <StyledBanner>
+      <img src={image} alt={title} />  //배너에 사진과 제목 넣기
+      <h2>{title}</h2>
+    </StyledBanner>
   );
 };
 
 export default Banner;
+
