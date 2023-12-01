cb82dc26c0d8f8b8d5b4c62882ef245d987d7ad1
SeJin317 <121839794+SeJin317@users.noreply.github.com>
2023-12-01 15:36:35 +0900

    Create NewPostFind.js

---
+++ src/pages/NewPostFind.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+// 제목 입력란
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const Label = styled.p`
+  margin-left: 5px;
+  margin-right: 15px;
+  white-space: nowrap; /* Prevent line breaks */
+  font-size: 20px;
+`;
+
+const TitleInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const TitleComponent = () => {
+  const [Title, setTitle] = useState('');
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div>
+      <TitleContainer>
+        <Label>제목</Label>
+        <TitleInput placeholder="제목을 입력하세요" onChange={onChangeTitle} value={Title} />
+      </TitleContainer>
+    </div>
+  );
+}
+
+export default TitleComponent;
+
+
+//연락 방법 체크란
+const Contact = () => {
+  const [RadioValue, setRadioValue] = useState('');
+  const radioarr = [
+    { value: 1, label: '위탁 장소에 문의' },
+    { value: 2, label: '오픈 채팅방' },
+    { value: 3, label: '기타' },
+    // Add more options as needed
+  ];
+
+  const handleRadio = (event) => {
+    setRadioValue(event.target.value);
+  };
+
+  return (
+    <div className="Radio">
+      <p>연락 방법</p>
+      {radioarr.map((section) => (
+        <div className="radio_button" key={section.value}>
+          <input
+            type="radio"
+            value={section.label}
+            onChange={handleRadio}
+            checked={RadioValue === section.label}
+          />
+          <label className="label">{section.label}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Contact;
